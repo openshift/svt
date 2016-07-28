3b34bb16746cd17d394d925ebfd8cc1222942a3a
Jiri Mencak <jmencak@redhat.com>
2016-07-28 14:27:06 +0200

    Removed command substitution from verify-registry.sh and verify-router.sh for the scripts to make them work.
    Signed-off-by: Jiri Mencak <jmencak@redhat.com>

--- openshift_scalability/scripts/verify-registry.sh
+++ openshift_scalability/scripts/verify-registry.sh
@@ -9,7 +9,7 @@ set -o nounset
 set -o pipefail
 
 # checks that docker-registry service exists in the 'default' namespace
-if ! $(oc adm registry --dry-run --namespace default); then
+if ! oc adm registry --dry-run --namespace default; then
     echo "docker-registry service doesn't exit"
     exit 1
 fi
@@ -18,7 +18,7 @@ fi
 REGISTRY_IP=$(oc get services --template '{{range .items}}{{if eq .metadata.name "docker-registry"}}{{printf "%s\n" .spec.clusterIP}}{{end}}{{end}}' --namespace default)
 
 # curl $REGISTRY_IP:5000 to verify docker-registry pod is running
-if ! $(curl --head --fail ${REGISTRY_IP}:5000); then
+if ! curl --head --fail ${REGISTRY_IP}:5000; then
     echo "docker-registry pod is not running"
     exit 1
 fi
