d978da43bf21c13c0fda4375eff4afbcdf6d681b
Vikas Laad <vlaad@redhat.com>
2017-01-10 15:01:31 -0500

    Scripts for running concurrent projects and builds

---
+++ openshift_performance/ci/scripts/conc_proj.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+old_i=1
+sed -i "s/num: .*/num: 1/g" ../content/conc_proj.yaml
+rm -rf ./conc_proj.out
+echo "#of Projects,Time (Sec)" >> conc_proj.out
+
+for i in 1 5 10 20 40 60 80 100 120 140
+do
+  sed -i "s/num: $old_i/num: $i/g" ../content/conc_proj.yaml
+
+  echo "Creating $i projects"
+  start_time=`date +%s`
+  python ../../../openshift_scalability/cluster-loader.py -p $i -f ../content/conc_proj.yaml
+  stop_time=`date +%s`
+
+  total_time=`echo $stop_time - $start_time | bc`
+  echo "Time taken for creating $i concurrent projects : $total_time"
+  echo "$i,$total_time" >> conc_proj.out
+
+  echo "Deleting $i projects"
+  oc delete project -l purpose=test
+
+  sleep 30
+  old_i=$i
+done
+cat conc_proj.out
