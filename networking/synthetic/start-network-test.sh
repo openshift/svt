0090d6fc56f919713c1940664ed5db34dafdd437
Siva Reddy Chitukula <schituku@redhat.com>
2017-01-17 13:34:44 -0500

    Enhancements to network tests: 	- add to the pbench the type of test: loopback, masterNode or nodeNode 	- wait for project delete in start script

--- networking/synthetic/start-network-test.sh
+++ networking/synthetic/start-network-test.sh
@@ -5,6 +5,23 @@
 ## Desription: Running network tests without manual involvement.
 ##
 ################################################
+
+function wait_for_project_delete {
+    uperfprojects=`oc get projects | grep uperf | wc -l`
+
+    while [ 0 -lt $uperfprojects ]
+    do
+     uperfprojects=`oc get projects | grep uperf | wc -l`
+     echo $uperfprojects
+     if [ 0 -eq $uperfprojects ]
+     then
+       break
+     else
+    	sleep 60
+     fi
+    done
+    }
+        
 cur_dir=`pwd`
 master=`cat config.yaml |egrep 'master:' | awk -F: '{print $2}'`
 nodes=`cat config.yaml |egrep 'nodes:' | awk -F: '{print $2}'`
@@ -21,26 +38,32 @@ do
 
     echo "INFO : $(date) #################### loopback on master for pod - $var pods ####################"
     python network-test.py podIP --master $master --pods $var
-    sleep 200
+    sleep 120
+    wait_for_project_delete
 
     echo "INFO : $(date) #################### loopback on master for svc - $var pods ####################"
     python network-test.py svcIP --master $master --pods $var
-    sleep 200
+    sleep 120
+    wait_for_project_delete
 
     echo "INFO : $(date) #################### cross host on master to node for pod - $var pods ####################"
     python network-test.py podIP --master $master --node ${nodes_array[0]} --pods $var
-    sleep 200
+    sleep 120
+    wait_for_project_delete
 
     echo "INFO : $(date) #################### cross host on master to node for svc - $var pods ####################"
     python network-test.py svcIP --master $master --node ${nodes_array[0]} --pods $var
-    sleep 200
-
+    sleep 120
+    wait_for_project_delete
+	
     echo "INFO : $(date) #################### cross host on node to node for pod - $var pods ####################"
     python network-test.py podIP --master $master --node ${nodes_array[0]} ${nodes_array[1]} --pods $var
-    sleep 200
+    sleep 120
+    wait_for_project_delete
 
     echo "INFO : $(date) #################### cross host on node to node for svc - $var pods ####################"
     python network-test.py svcIP --master $master --node ${nodes_array[0]} ${nodes_array[1]} --pods $var
-    sleep 200
+    sleep 120
+    wait_for_project_delete
 
 done
