1a6f6609eeadd33e72c09d26c96dfc3cfa291df9
Hongkai Liu <hongkailiu@users.noreply.github.com>
2017-07-17 10:30:03 -0400

    Remove checking on private registry

--- logging_metrics_performance/scripts/check_images_logging_and_metrics.sh
+++ logging_metrics_performance/scripts/check_images_logging_and_metrics.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ### check if the desired version of logging/metrics is ready for test
-### require jq command: 'yum install jq' if command not found
 
 function print_usage {
   echo "usage: $0 <version>"
@@ -17,31 +16,6 @@ readonly DATE="$(date +%Y-%m-%dT%H:%M:%S)"
 echo ${DATE}
 
 readonly IMAGE_VERSION=$1
-
-function check_brew_one(){
-  local image
-  image=$1
-  local version
-  version=$2
-  while read version_on_server;
-  do
-    if [[ "${IMAGE_VERSION}" == "${version_on_server}" ]]; then
-      echo "VVV:image $image version $version found in brew."
-      return
-    fi
-  done << EOF
-$(curl -s -k brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/v1/repositories/openshift3/${image}/tags | jq 'keys' | jq -r .[] | grep ${version})
-EOF
-  echo "XXX:image $image version $version not found in brew!"
-}
-
-
-function check_brew() {
-  for image in "${logging_image_array[@]}"
-  do
-    check_brew_one "${image}" "${IMAGE_VERSION}"
-  done
-}
 
 function check_registry_one(){
   local image
@@ -66,14 +40,6 @@ function check_registry() {
 
 # logging-mux uses "logging-fluentd" image
 declare -a logging_image_array=("logging-kibana" "logging-curator" "logging-elasticsearch" "logging-fluentd")
-
-ping -q -c 1 brew-pulp-docker01.web.prod.ext.phx2.redhat.com > /dev/null
-
-if [[ $? -eq 0 ]]; then
-  check_brew
-else
-  echo "skip check_brew"
-fi
 
 check_registry
 
