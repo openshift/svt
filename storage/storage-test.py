b862ac56508b2d476b88aaba78f6e2cd4d168401
Siva Reddy Chitukula <schituku@redhat.com>
2018-03-13 14:37:33 -0400

    - fixes that will enable the scripts to run for ansible 2.4

--- storage/storage-test.py
+++ storage/storage-test.py
@@ -5,49 +5,49 @@ import json
 from collections import namedtuple
 
 from ansible.parsing.dataloader import DataLoader
-from ansible.vars import VariableManager
-from ansible.inventory import Inventory
+from ansible.vars.manager import VariableManager
 from ansible.executor.playbook_executor import PlaybookExecutor
 
 from ansible import utils
-from ansible.inventory import Inventory
-from ansible.inventory.group import Group
-from ansible.inventory.host import Host
+from ansible.inventory.manager import InventoryManager
 
 class StorageTest(object):
     def __init__(self, playbook):
-        self.variable_manager = VariableManager()
         self.loader = DataLoader()
-        self.inventory = Inventory(loader=self.loader, variable_manager=self.variable_manager,host_list=[])
-        self.variable_manager.set_inventory(self.inventory)
+        self.inventory = InventoryManager(loader=self.loader, sources=None)
+        self.variable_manager = VariableManager(self.loader,self.inventory)
         self.playbook = playbook
         
-        self.sender_group = Group(name = 'sender')
+        self.sender_group = 'sender'
         self.inventory.add_group(self.sender_group)
         
-        self.receiver_group = Group(name = 'receiver')
+        self.receiver_group = 'receiver'
         self.inventory.add_group(self.receiver_group)
         
-        self.master_group = Group(name = 'master')
+        self.master_group = 'master'
         self.inventory.add_group(self.master_group)
         
     def set_inventory_vars(self, inv_vars):
         self.variable_manager.extra_vars = inv_vars
 
 
+    def set_inventory_vars(self, inv_vars):
+        self.variable_manager.extra_vars = inv_vars
+
+
     def add_sender(self, sender):
-        sender_host = Host(name = sender)
-        self.sender_group.add_host(sender_host)
+        sender_host =  sender
+        self.inventory.add_host(sender_host, self.sender_group)
 
 
     def add_receiver(self, receiver):
-        receiver_host = Host(name = receiver)
-        self.receiver_group.add_host(receiver_host)
+        receiver_host = receiver
+        self.inventory.add_host(receiver_host, self.receiver_group)
 
         
     def add_master(self, master):
-        master_host = Host(name = master)
-        self.master_group.add_host(master_host)
+        master_host = master
+        self.inventory.add_host(master_host, self.master_group)
 
 
     def run(self):
@@ -57,14 +57,15 @@ class StorageTest(object):
                               'forks', 'remote_user', 'private_key_file', 
                               'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 
                               'scp_extra_args', 'become', 'become_method', 
-                              'become_user', 'verbosity', 'check'])
+                              'become_user', 'verbosity', 'check', 'diff'])
         options = Options(listtags=False, listtasks=False, listhosts=False, 
                           syntax=False, connection='ssh', module_path=None, 
                           forks=100, remote_user='root', private_key_file=None, 
                           ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, 
                           scp_extra_args=None, become=True, become_method=None, 
-                          become_user='root', verbosity=None, check=False)
-
+                          become_user='root', verbosity=None, check=False, diff=False)
+        #OPTION_FLAGS = ('connection', 'remote_user', 'private_key_file', 'verbosity', 'force_handlers', 'step', 'start_at_task', 'diff',
+        #        'ssh_common_args', 'docker_extra_args', 'sftp_extra_args', 'scp_extra_args', 'ssh_extra_args')
         passwords = {}
 
         pbex = PlaybookExecutor(playbooks=[self.playbook], 
@@ -124,13 +125,11 @@ def set_receiver(master, nodes):
         return nodes[1]
     
 
-def set_pbench_remote(master, nodes):
+def set_pbench_remotes(master, nodes):
     if nodes is None:
         return 'None'
-    elif len(nodes) == 1:
-        return nodes[0]
-    elif len(nodes) == 2:
-        return nodes[1]
+    elif len(nodes) >= 1:
+        return nodes
     
 
 def set_sender_region(master, nodes):
@@ -163,7 +162,7 @@ def main():
     receiver_host = set_receiver(args.test_master, args.test_nodes)
     master_host = args.test_master
     
-    pbench_remote = set_pbench_remote(args.test_master, args.test_nodes)
+    pbench_remotes = set_pbench_remotes(args.test_master, args.test_nodes)
     
     sender_region = set_sender_region(args.test_master, args.test_nodes)
     receiver_region = set_receiver_region(args.test_master, args.test_nodes)
@@ -177,7 +176,7 @@ def main():
                        'receiver_region': receiver_region,
                        'project_number': project_number,
                        'pbench_label': pbench_label,
-                       'pbench_remote': pbench_remote }
+                       'pbench_remotes': pbench_remotes }
     
     nettest = StorageTest(test_playbook)
 
