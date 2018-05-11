eb02c40394d6dd617d14656be91632aee314ff0e
Naga Ravi Chaitanya Elluri <naga.elluri@colorado.edu>
2018-05-10 22:56:43 -0400

    Add support for nodevertical to schedule pods on labeled nodes
    - This will schedule the pods on nodes with a nodevertical=true label.
      This is needed to hit the max pod count on a node when the cluster
      size is greater than 4.
    - This also calculates the number of pods running on the selected nodes
      and modifies the pod number in the config to avoid creating more pods
      than supported.
    - This change supports labeling nodes both on Alderaan and AWS
      environments.

--- openshift_scalability/nodeVertical.sh
+++ openshift_scalability/nodeVertical.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]; then
-  echo "syntax: $0 <TESTNAME> <TYPE>"
+if [ "$#" -ne 3 ]; then
+  echo "syntax: $0 <TESTNAME> <TYPE> <ENVIRONMENT>"
   echo "<TYPE> should be either golang or python"
+  echo "<ENVIRONMENT> should be either alderaan or aws"
   exit 1
 fi
 
 TESTNAME=$1
 TYPE=$2
+ENVIRONMENT=$3
+LABEL="node-role.kubernetes.io/compute=true"
+CORE_COMPUTE_LABEL="core_app_node=true"
+TEST_LABEL="nodevertical=true"
+declare -a CORE_NODES
+NODE_COUNT=0
+pod_count=0
 
 long_sleep() {
   local sleep_time=180
@@ -20,7 +28,13 @@ clean() { echo "Cleaning environment"; oc delete project clusterproject0; }
 golang_clusterloader() {
   # Export kube config
   export KUBECONFIG=${KUBECONFIG-$HOME/.kube/config}
-  MY_CONFIG=config/golang/nodeVertical
+  if [[ "$ENVIRONMENT" == "alderaan" ]]; then
+  	MY_CONFIG=config/golang/nodeVertical-labeled-nodes
+	sed -i "/- num: 1000/c \ \ \ \ \ \ \ \ \- num: $total_pod_count" /root/svt/openshift_scalability/config/golang/nodeVertical-labeled-nodes.yaml
+  else
+  	MY_CONFIG=config/golang/nodeVertical
+	sed -i "/- num: 1000/c \ \ \ \ \ \ \ \ \- num: $total_pod_count" /root/svt/openshift_scalability/config/golang/nodeVertical.yaml
+  fi
   # loading cluster based on yaml config file
   /usr/libexec/atomic-openshift/extended.test --ginkgo.focus="Load cluster" --viper-config=$MY_CONFIG
 }
@@ -32,6 +46,46 @@ python_clusterloader() {
 
 # sleeping to gather some steady-state metrics, pre-test
 long_sleep
+
+# set the number of nodes to label based on environment selected
+if [[ "$ENVIRONMENT" == "alderaan" ]]; then
+	LABEL_COUNT=2
+else
+	LABEL_COUNT=4
+fi
+
+# label the core nodes when using Alderaan env
+if [[ "$ENVIRONMENT" == "alderaan" ]]; then
+	for compute in $(oc get nodes -l "$CORE_COMPUTE_LABEL" -o json | jq '.items[].metadata.name'); do
+        	compute=$(echo $compute | sed "s/\"//g")
+        	CORE_NODES[${#CORE_NODES[@]}]=$compute
+        	oc label node $compute "$TEST_LABEL"
+	done
+fi
+
+# pick two random app nodes and label them
+for app_node in $(oc get nodes -l "$LABEL" -o json | jq '.items[].metadata.name'); do
+	app_node=$(echo $app_node | sed "s/\"//g")
+	if [[ "$ENVIRONMENT" == "alderaan" ]]; then
+		if [[ ! $(echo ${CORE_NODES[@]} | grep -q -w $app_node) ]]; then
+			NODE_COUNT=$(( NODE_COUNT+1 ))
+			oc label node $app_node "$TEST_LABEL"
+		fi
+	else
+		NODE_COUNT=$(( NODE_COUNT+1 ))
+		oc label node $app_node "$TEST_LABEL"
+	fi
+	if [[ $NODE_COUNT -ge $LABEL_COUNT  ]]; then
+		break
+	fi
+done
+
+# Get the pod count on the labeled nodes
+for node in $(oc get nodes -l="nodevertical=true" | awk 'NR > 1 {print $1}'); do
+	pods_running=$(oc describe node $node | grep -w "Non-terminated \Pods:" | awk  '{print $3}' | sed "s/(//g")
+	pod_count=$(( pod_count+pods_running ))
+done
+total_pod_count=$(( 1000-pod_count ))
 
 # Run the test
 if [ "$TYPE" == "golang" ]; then
