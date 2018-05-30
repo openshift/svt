4da83b7899f6662ae3b38d9fb59588e7cee58fd3
Elvir Kuric <elvirkuric@gmail.com>
2018-05-30 11:00:24 +0200

    external_vars.yaml added recordcount and operationcount options files/scripts/test-mongo.sh - sorted results and write them to .csv files for easier drawing afterwards mongodb-test.yaml - some aditions for pbench-user-benchmark --config section runmongo.sh - wrapper script which helps to run multiple tests in one run
    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>

--- storage/mongodb/files/scripts/test-mongo.sh
+++ storage/mongodb/files/scripts/test-mongo.sh
@@ -6,6 +6,8 @@ readonly NAMESPACE=${1}
 readonly ITERATION=${2}
 readonly THREADS=${3}
 readonly WORKLOAD=${4}
+readonly RECORDCOUNT=${5}
+readonly OPERATIONCOUNT=${6}
 
 output_dir=$(dirname $0)
 
@@ -19,17 +21,33 @@ echo "NAMESPACE: ${NAMESPACE}"
 echo "ITERATION: ${ITERATION}"
 echo "THREADS: ${THREADS}"
 echo "WORKLOADS: ${WORKLOAD}"
+echo "RECORDCOUNT: ${RECORDCOUNT}"
+echo "OPERATIONCOUNT: ${OPERATIONCOUNT}"
 
-for i in $(seq 1 ${ITERATION});
-do
-  for load  in $(echo ${WORKLOAD} | sed -e s/,/" "/g);
-  do
-    echo "iteration: ${i} "and" ${load}"
-    ## TODO support to override other params
-    oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo -u redhat -p redhat ${MONGODB_IP}:27017/testdb --eval "db.usertable.remove({})"
-    oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads "${THREADS}" -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb > ${output_dir}/mongodb_${load}_result_run_${THREADS}_${i}.txt 2>&1
-    grep -E 'RunTime|Throughput' ${output_dir}/mongodb_${load}_result_run_${THREADS}_${i}.txt > ${output_dir}/mongodb_${load}_result_run_${THREADS}_${i}_brief.txt
-    grep Throughput ${output_dir}/mongodb_${load}_result_run_${THREADS}_${i}.txt | cut -d',' -f3 >> ${output_dir}/mongodb_${load}_result_run_${THREADS}_numbers.txt
-  done
+
+for i in $(seq 1 ${ITERATION}); do 
+  for load  in $(echo ${WORKLOAD} | sed -e s/,/" "/g); do 
+	for thread in $(echo ${THREADS} | sed -e s/,/" "/g); do 
+    		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo -u redhat -p redhat ${MONGODB_IP}:27017/testdb --eval "db.usertable.remove({})"
+		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} 2>&1 | tee -a ${output_dir}/mongodb_load_data_${load}_threads_${thread}.txt
+
+		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb run mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT}  2>&1 | tee -a ${output_dir}/mongodb_run_load_${load}_threads_${thread}.txt
+	done 
+   done 
 done
+
+# sort result 
+
+for load  in $(echo ${WORKLOAD} | sed -e s/,/" "/g); do
+	for thread in $(echo ${THREADS} | sed -e s/,/" "/g); do
+		echo "Threads-${thread}" > ${output_dir}/result_${load}_threads_${thread}.txt 
+		grep Throughput ${output_dir}/mongodb_run_load_${load}_threads_${thread}.txt | cut -d',' -f3 | cut -d' ' -f2 >> ${output_dir}/result_${load}_threads_${thread}.txt
+	done
+	paste -d',' ${output_dir}/result_${load}_threads_* > ${output_dir}/result_${load}_recordcount_${RECORDCOUNT}_operationcount_${OPERATIONCOUNT}.csv
+done 
+
+
+
+
+# todo - draw results here ... 
 
