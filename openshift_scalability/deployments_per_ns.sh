b4d2c6a1c1b0807980802db1eacf2d8a0b7869a7
Naga Ravi Chaitanya Elluri <naga.elluri@colorado.edu>
2018-07-02 15:10:12 -0400

    Add deployments per namespace cluster limit test (#480)
    This test creates a single namespace with N deployments. The
    deploymentConfig template creates 1 replica and 4 env vars.

---
+++ openshift_scalability/deployments_per_ns.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+  echo "syntax: $0 <TYPE>"
+  echo "<TYPE> should be either golang or python"
+  exit 1
+fi
+
+TYPE=$1
+
+long_sleep() {
+  local sleep_time=180
+  echo "Sleeping for $sleep_time"
+  sleep $sleep_time
+}
+
+golang_clusterloader() {
+  # Export kube config
+  export KUBECONFIG=${KUBECONFIG-$HOME/.kube/config}
+  MY_CONFIG=config/golang/cluster-limits-deployments-per-namespace
+  # loading cluster based on yaml config file
+  /usr/libexec/atomic-openshift/extended.test --ginkgo.focus="Load cluster" --viper-config=$MY_CONFIG
+}
+
+python_clusterloader() {
+  MY_CONFIG=config/cluster-limits-deployments-per-namespace.yaml
+  ./cluster-loader.py --file=$MY_CONFIG
+}
+
+# sleeping to gather some steady-state metrics, pre-test
+long_sleep
+
+# Run the test
+if [ "$TYPE" == "golang" ]; then
+  golang_clusterloader
+elif [ "$TYPE" == "python" ]; then
+  python_clusterloader
+  # sleeping again to gather steady-state metrics after environment is loaded
+  long_sleep
+else
+  echo "$TYPE is not a valid option, available options: golang, python"
+  exit 1
+fi
+
+# sleep after test is complete to gather post-test metrics...these should be the same as pre-test
+long_sleep
