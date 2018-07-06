64b346af0ea37aef869faeb7002ab404c29b71d1
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-07-06 14:14:26 -0400

    Disable host key checking in fio script

--- storage/fio/files/scripts/drop-cache.sh
+++ storage/fio/files/scripts/drop-cache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-oc get nodes --no-headers | cut -f1 -d" " | while read i; do ssh -n "$i" 'sync ; echo 3 > /proc/sys/vm/drop_caches'; done
+oc get nodes --no-headers | cut -f1 -d" " | while read i; do ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n "$i" 'sync ; echo 3 > /proc/sys/vm/drop_caches'; done
