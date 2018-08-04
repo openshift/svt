c5a8d9732e62b928a7c0c783867045b4e87e780e
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-08-04 07:41:02 -0400

    Use pbench status as a separate step in fio test

--- storage/fio/files/scripts/test-storage.sh
+++ storage/fio/files/scripts/test-storage.sh
@@ -2,26 +2,15 @@
 
 echo "001 $(date)"
 
-if [ "$#" -ne 1 ]; then
-    echo "need the path of tmp folder"
+if [ "$#" -ne 2 ]; then
+    echo "need the path of tmp folder and STORAGE_CLASS_NAME"
     exit 1
 fi
 
 readonly TMP_FOLDER=$1
+readonly STORAGE_CLASS_NAME=$2
 
-pbench-kill-tools
-pbench-clear-tools
-pbench-clear-results
-
-pbench-register-tool-set --label=FIO
-pbench-register-tool --name=oc
-
-readonly NODES=($(oc get node  --no-headers | grep -v master | awk '{print $1}'))
-
-for node in "${NODES[@]}"
-do
-  pbench-register-tool-set --label=FIO --remote="${node}"
-done
+### pbench-register runs before the test is started if necessary
 
 echo "002 $(date)"
 
@@ -31,15 +20,13 @@ readonly CLIENT_HOSTS="${CLIENT_HOSTS_COMMA::-1}"
 
 echo "CLIENT_HOST ${CLIENT_HOSTS}"
 
-readonly CONFIG_SUFFIX=""
+#pbench-fio --test-types=read --clients="${CLIENT_HOSTS}" --config="SEQ_IO_${STORAGE_CLASS_NAME}" --samples=1 --max-stddev=20 --block-sizes=4 --job-file="${TMP_FOLDER}/config/sequential_io.job" --pre-iteration-script="${TMP_FOLDER}/scripts/drop-cache.sh"
 
-#pbench-fio --test-types=read --clients="${CLIENT_HOSTS}" --config="SEQ_IO${CONFIG_SUFFIX}" --samples=1 --max-stddev=20 --block-sizes=4 --job-file="${TMP_FOLDER}/config/sequential_io.job" --pre-iteration-script="${TMP_FOLDER}/scripts/drop-cache.sh"
-
-pbench-fio --test-types=read,write,rw --clients="${CLIENT_HOSTS}" --config="SEQ_IO${CONFIG_SUFFIX}" --samples=1 --max-stddev=20 --block-sizes=4,16,64 --job-file="${TMP_FOLDER}/config/sequential_io.job" --pre-iteration-script="${TMP_FOLDER}/scripts/drop-cache.sh"
+pbench-fio --test-types=read,write,rw --clients="${CLIENT_HOSTS}" --config="SEQ_IO_${STORAGE_CLASS_NAME}" --samples=1 --max-stddev=20 --block-sizes=4,16,64 --job-file="${TMP_FOLDER}/config/sequential_io.job" --pre-iteration-script="${TMP_FOLDER}/scripts/drop-cache.sh"
 
 echo "003 $(date)"
 
-pbench-fio --test-types=randread,randwrite,randrw --clients="${CLIENT_HOSTS}" --config="RAND_IO${CONFIG_SUFFIX}" --samples=1 --max-stddev=20 --block-sizes=4,16,64 --job-file="${TMP_FOLDER}/config/random_io.job" --pre-iteration-script="${TMP_FOLDER}/scripts/drop-cache.sh"
+pbench-fio --test-types=randread,randwrite,randrw --clients="${CLIENT_HOSTS}" --config="RAND_IO_${STORAGE_CLASS_NAME}" --samples=1 --max-stddev=20 --block-sizes=4,16,64 --job-file="${TMP_FOLDER}/config/random_io.job" --pre-iteration-script="${TMP_FOLDER}/scripts/drop-cache.sh"
 
 echo "pbench-copy-results: $(date)"
 
