b39fce502e0eccdcd819963445d250186f53196f
Eldad Marciano <mrsiano@gmail.com>
2018-08-09 13:45:37 +0300

    dynamic_loader

--- openshift_scalability/ci/scripts/prometheus/prometheus-loader.py
+++ openshift_scalability/ci/scripts/prometheus/prometheus-loader.py
@@ -4,17 +4,20 @@
 An loader tool to simulate grafana dashboard queries against prometheus.
 
 Usage:
-python prometheusLoader.py -f ./qs.txt -i 20 -t 50 -p 15  > /dev/null 2>&1 &
+python prometheusLoader.py -i 20 -t 50 -p 15
 
 Log:
 /tmp/prometheus_loader.log
 """
+import argparse
 import logging
 import os
+import sys
 import requests
 import random
 import time
-import argparse
+import threading
+from loaddashboards import Dashboards
 from requests.utils import quote
 from concurrent.futures import ThreadPoolExecutor
 
@@ -28,13 +31,13 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f',
                         '--file',
-                        required=True,
+                        required=False,
                         dest='file',
                         help='queries file')
 
     parser.add_argument('-t',
                         '--threads',
-                        required=True,
+                        required=False,
                         type=int,
                         dest='threads',
                         help='simultaneously requests')
@@ -75,12 +78,19 @@ def parse_args():
                         dest='sa',
                         help='promethues service_account')
 
+    parser.add_argument('-y',
+                        '--yaml',
+                        type=str,
+                        required=False,
+                        default='https://raw.githubusercontent.com/openshift/cluster-monitoring-operator/master/assets/grafana/dashboard-definitions.yaml',
+                        dest='yaml',
+                        help='gitrepo')
+
     return parser.parse_args()
 
 class PrometheusLoader(object):
-    def __init__(self, file, threads, period, resolution, ns, sa):
+    def __init__(self, file, threads, period, resolution, ns, sa, yaml, interval):
         # args
-        self.queries_file = file
         self.threads = threads
         self.period = period
         self.queries = []
@@ -97,15 +107,27 @@ class PrometheusLoader(object):
         self.query = ""
         self.ns = ns
         self.sa = sa
-        
+        self.dashboardname = ""
+        self.con = 0
+        self.interval = interval
         self.logger()
-        self.read_queries_from_file()
         self.get_prometheus_info()
-        
+        self.lock = threading.Lock()
+
+        # stepping computation
         if resolution is None:
             self.steping = self.compute_stepping()
         else:
-            self.steping = resolution        
+            self.steping = resolution
+
+        # dashboard loader
+        if yaml:
+            self.load_queries_from_source(yaml)
+        elif file:
+            self.load_queries_from_file(file)
+        else:
+            self.log.error('yaml or file should given')
+            sys.exit(os.EX_CONFIG)
 
     def logger(self):
         try:
@@ -117,13 +139,15 @@ class PrometheusLoader(object):
             self.log.error('failed to start log {0} - {1}'.format(
                 self.log_file, e))
 
-    def read_queries_from_file(self):
-        """
-        read queries from file and qute it as urlencoded
-        """
-        with open(self.queries_file, 'r') as f:
+    def load_queries_from_file(self, file):
+        ''' load and qute queries from text file '''
+        with open(file, 'r') as f:
             for line in f:
                 self.queries.append(quote(line))
+
+    def load_queries_from_source(self, yaml):
+        ''' load and qute queries from source (yaml mixin) '''
+        self.dashboards = Dashboards(yaml).get_dashboards()
 
     def get_prometheus_info(self):
         ''' get token and route for prometeus '''
@@ -144,31 +168,40 @@ class PrometheusLoader(object):
                                           ).read().rstrip()
 
 
-    def generate_req(self):
+    def generate_req(self, q):
         ''' gernerate query as http format '''
+
         time_from = os.popen('date "+%s" -d "{0} min ago"'.format(
                                 self.period)).read().rstrip()
         time_now = os.popen('date "+%s"').read().rstrip()
-        self.query = self.queries[random.randint(1, len(self.queries) - 1)]
+        # lock the q(query) in order to sync the log.
+        self.lock.acquire()
+        self.query = q
         return "https://{0}/api/v1/query_range?query={1}&start={2}&end={3}" \
             "&step={4}".format(self.promethues_server,
-                             self.query, time_from, time_now, self.steping)
+                             q, time_from, time_now, self.steping)
 
     def request(self, req):
         ''' fire http request '''
+        reqinfo = ' [{0}] - concurrency:{1} - query:{2}'.format(
+                                            self.dashboardname,
+                                            self.con,
+                                            self.query)
+        # release the lock.
+        self.lock.release()
         try:
             res = requests.get(req, verify=False, headers=self.headers)
         except (IOError, RequestException) as e:
-            self.log.error('bad request {0} response {1}'.format(req, e))
+            self.log.error('bad request {0} response {1}'.format(reqinfo, e))
         if len(res.text) == 0:
-            self.log.error('bad request {0} response {1}'.format(req, res))
+            self.log.error('bad request {0} response {1}'.format(reqinfo, res))
         self.log.info('duration: {0} - {1}'.format(res.elapsed.total_seconds(),
-                                                self.query))
+                                                reqinfo))
 
-    def run_loader(self):
+    def run_loader(self, qs):
         ''' fire http requests simultaneously in threads batch '''
-        for i in range(self.threads):
-            self.executor.submit(self.request, self.generate_req())
+        for query in queries:
+            self.executor.submit(self.request, self.generate_req(query))
 
     def health_collector(self):
         # check promethues scrapping success rate
@@ -194,11 +227,31 @@ class PrometheusLoader(object):
         else:
             return 15
 
+    def dashboard_loader(self):
+        if self.queries:
+            self.run_loader(self.queries)
+            time.sleep(self.interval)
+        elif self.dashboards:
+            # iterate all dashboards with pause of interval
+            for dashboard in self.dashboards:
+                self.dashboardname = dashboard['name']
+                self.con = len(dashboard['queries'])
+                self.executor = ThreadPoolExecutor(max_workers=self.con)
+                self.run_loader(dashboard['queries'])
+                time.sleep(self.interval)
+        else:
+            self.log.error('no dashboard or queries were found')
+            sys.exit(os.EX_CONFIG)
+
+    # start the loader.
+    def start(self):
+        while True:
+            self.dashboard_loader()
+            self.health_collector()
+
 if __name__ == "__main__":
     args = parse_args()
     p = PrometheusLoader(args.file, args.threads, args.period, args.resolution,
-                         args.ns, args.sa)
-    while True:
-        p.run_loader()
-        p.health_collector()
-        time.sleep(args.interval)
+                         args.ns, args.sa, args.yaml, args.interval)
+    # start the loader.
+    p.start()
