7a45ac94cf03aa44a09c4ec492c1066d81383a29
Eldad Marciano <mrsiano@gmail.com>
2018-10-09 13:16:35 +0300

    promethues workloads update

--- openshift_scalability/ci/scripts/prometheus/sanity-period-performance.sh
+++ openshift_scalability/ci/scripts/prometheus/sanity-period-performance.sh
@@ -8,8 +8,6 @@ enable_pbench=$4
 pbench_copy_results=$5
 pbench_user_benchmark=$6
 
-# replace with init script
-oc login -u system:admin
 if [ "${enable_pbench}" == "true" ]; then
     for n in `oc get no |grep -v app |awk '{print $1}'`; do pbench-register-tool-set --remote=$n; done
 fi
@@ -18,7 +16,7 @@ function sanity_period() {
   period=$1
   enable_pbench=$2
   pbench_user_benchmark=$(echo ${pbench_user_benchmark} |sed -e "s|PERIOD${GRAPH_PERIOD} --|PERIOD${period} --|")
-  ./prometheus-loader.sh 20 20 ${period} ${duration} ${enable_pbench} ${pbench_copy_results} ${pbench_user_benchmark}
+  ./prometheus-loader.sh 0 0 ${period} ${duration} ${enable_pbench} ${pbench_copy_results} ${pbench_user_benchmark}
 }
 
 # last 15min
