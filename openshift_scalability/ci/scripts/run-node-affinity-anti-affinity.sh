b4d7110a212f35d7c8ecd52d6247d1c2ed7f9cc3
Walid Abouhamad <wabouham@redhat.com>
2019-08-27 07:59:12 -0400

    Updated scripts run-node-affinity-anti-affinity.sh taint_nodes.sh for OCP 4.2, and renamed config files with updated pod numbers node-affinity.yaml and taints-tolerations.yaml

--- openshift_scalability/ci/scripts/run-node-affinity-anti-affinity.sh
+++ openshift_scalability/ci/scripts/run-node-affinity-anti-affinity.sh
@@ -8,16 +8,15 @@ oc version
 oc get node --show-labels
 oc describe node | grep Runtime
 
+compute_nodes=$(oc get nodes -l 'node-role.kubernetes.io/worker=' | awk '{print $1}' | grep -v NAME | xargs)
 
-compute_nodes=$(oc get nodes -l 'node-role.kubernetes.io/compute=true' | awk '{print $1}' | grep -v NAME | xargs)
-
-echo -e "\nComputes nodes are: $compute_nodes"
+echo -e "\nWorker  nodes are: $compute_nodes"
 
 declare -a node_array
 counter=1
 
-oc get nodes -l 'node-role.kubernetes.io/compute=true'
-oc describe nodes -l 'node-role.kubernetes.io/compute=true' 
+oc get nodes -l 'node-role.kubernetes.io/worker='
+oc describe nodes -l 'node-role.kubernetes.io/worker=' 
 
 initial_node_label="beta.kubernetes.io/arch=amd64"
 
@@ -71,8 +70,17 @@ export KUBECONFIG=${KUBECONFIG-$HOME/.kube/config}
 cd /root/svt/openshift_scalability
 ls -ltr config/golang
 
-echo -e "\nStarting GoLang cluster-loader with config file: config/golang/node-affinity-anti-affinity"
-/usr/libexec/atomic-openshift/extended.test --ginkgo.focus="Load cluster" --viper-config=config/golang/node-affinity-anti-affinity
+#### OCP 4.2: new requirements to run golang cluster-loader from openshift-tests binary:
+## - Absolute path to config file needed
+## - .yaml extension is required now in config file name
+## - full path to the config file  must be under 70 characters total
+
+MY_CONFIG=/root/svt/openshift_scalability/config/golang/node-affinity.yaml
+echo -e "\nRunning GoLang cluster-loader from openshift-tests binary with config file: ${MY_CONFIG}"
+echo -e "\nContents of  config file: ${MY_CONFIG}"
+cat ${MY_CONFIG}
+
+VIPERCONFIG=$MY_CONFIG openshift-tests run-test "[Feature:Performance][Serial][Slow] Load cluster should load the cluster [Suite:openshift]"
 
 sleep 30
 
@@ -80,11 +88,11 @@ check_no_error_pods
 
 oc get pods --all-namespaces -o wide
 
-## TO DO:  check counts and later chage 10 to 250 in cluster loader yaml file
+## TO DO:  check pod counts expecting 130 pods per namespace
 oc get pods -n node-affinity-0 -o wide | grep "pausepods" | grep ${node_array[2]} | grep Running | wc -l
 oc get pods -n node-anti-affinity-0 -o wide | grep "hellopods" | grep ${node_array[1]} | grep Running | wc -l
 
-sleep 30
+sleep 60
 
 # delete projects:  cleanup
 oc delete project node-affinity-0
@@ -96,7 +104,6 @@ oc delete project node-anti-affinity-0
 sleep 30
 
 ## remove node labels
-
 echo -e "\nRemoving the node labels"
 oc label nodes ${node_array[1]} cpu-
 oc label nodes ${node_array[2]} cpu-
