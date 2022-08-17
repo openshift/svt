0610b1dfffabeb3aa10d69bbac23b81f28849a90
Qiujie Li <qili@redhat.com>
2022-08-17 11:24:26 +0800

    add parameter to large-network-policy

--- perfscale_regression_ci/scripts/network/large_network_policy_env.sh
+++ perfscale_regression_ci/scripts/network/large_network_policy_env.sh
@@ -1,6 +1,6 @@
 # WORKLOAD_TEMPLATE for custom workload of kube-burner
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export WORKLOAD_TEMPLATE=${WORKLOAD_TEMPLATE:-"${DIR}/../kubeburner-object-templates/pause-config.yml"}
+export WORKLOAD_TEMPLATE=${WORKLOAD_TEMPLATE:-"${DIR}/../../kubeburner-object-templates/pause-config.yml"}
 # ENVs to overwrite the kube-burner configuration file
 export NAME=${NAME:-"large-network-policy"}
 export NAMESPACE=${NAMESPACE:-"large-network-policy"}
@@ -9,4 +9,4 @@ export QPS=${QPS:-50}
 export BURST=${BURST:-50}
 export SERVICE_TYPE=${SERVICE_TYPE:-"ClusterIP"}
 # Other ENV needed by the test case script
-export NETWORK_POLICY=${NETWORK_POLICY:-"${DIR}/../content/allow_default_network_policy.yaml"}
+export NETWORK_POLICY=${NETWORK_POLICY:-"${DIR}/../../content/allow_default_network_policy.yaml"}
