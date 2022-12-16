5c0108996c0cb47283db30996d2c24dc5fea6bf5
Paige Rubendall <prubenda@redhat.com>
2022-12-16 10:50:30 -0500

    adding evict pvc script

---
+++ perfscale_regression_ci/scripts/scalability/descheduler/evict_pvc_env.sh
@@ -0,0 +1,8 @@
+# WORKLOAD_TEMPLATE for custom workload of kube-burner
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export WORKLOAD_TEMPLATE=${WORKLOAD_TEMPLATE:-"${DIR}/../../../kubeburner-object-templates/descheduler-evict-pvc.yml"}
+# ENVs to overwrite the kube-burner configuration file
+export NAME=${NAME:-"evict-pvc-desched"}
+export NAMESPACE=${NAMESPACE:-"evict-pvc-desched"}
+export WAIT_FOR=["PersistentVolumeClaim","ReplicationController"]
+export JOB_ITERATION=${JOB_ITERATION:=1}
