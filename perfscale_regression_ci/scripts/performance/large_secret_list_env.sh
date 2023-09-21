a7ecbc1e21185986e57aff2b622e353028dcbffc
Paige Rubendall <prubenda@redhat.com>
2023-09-21 15:57:44 -0400

    adding secrets

---
+++ perfscale_regression_ci/scripts/performance/large_secret_list_env.sh
@@ -0,0 +1,7 @@
+# ENVs to overwrite the kube-burner configuration file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export WORKLOAD_TEMPLATE="${DIR}/../../kubeburner-object-templates/secret-projects-config.yml"
+export NAME=${NAME:-"large-secrets"}
+export NAMESPACE=${NAMESPACE:-"large-secrets"}
+export JOB_ITERATION=${PARAMETERS:=100}
+export SECRET_REPLICAS=10
