f65123cd1e528049f92090faa09df1545d54daae
Qiujie Li <81630527+qiliRedHat@users.noreply.github.com>
2025-01-20 17:06:57 +0800

    add ingress tunning options regression test case (#804)

--- perfscale_regression_ci/scripts/scalability/router-scale/ingress-scale.sh
+++ perfscale_regression_ci/scripts/scalability/router-scale/ingress-scale.sh
@@ -5,17 +5,12 @@
 ## Polarion test case: OCP-43281
 ## https://polarion.engineering.redhat.com/polarion/#/project/OSE/workitem?id=OCP-43281
 ## Cluster config: 3 master (m5.xlarge), 9 workers(m5.2xlarge), 3 infras(c5.4xlarge), do not move component to infra nodes before the test
-## ingress-perf config: standard-3replicas.yml, standard-4replicas.yml
-## optional PARAMETERS: number of JOB_ITERATION
+## ingress-perf config: standard-3replicas.yml, standard-4replicas.yml, standard-3replicas-8threads.yml,standard-4replicas-8threads.yml
 ################################################ 
 set -o errexit
 
 source ../../common.sh
 source ../../../utils/run_workload.sh
-
-echo "[INFO] Patch threadCount as 4"
-oc -n openshift-ingress-operator patch ingresscontroller/default --type=merge -p '{"spec":{"tuningOptions": {"threadCount": 4}}}'
-oc -n openshift-ingress get deploy router-default -o yaml | grep " ROUTER_THREADS" -A 1
 
 echo "[INFO] run ingress-perf with thread=4, replica=2"
 run_ingress_perf
@@ -32,16 +27,12 @@ echo "[INFO] run ingress-perf with thread=4, replica=4"
 export CONFIG=../../../standard-4replicas.yml
 run_ingress_perf
 
-echo "[INFO] Patch threadCount as 8"
-oc -n openshift-ingress-operator patch ingresscontroller/default --type=merge -p '{"spec":{"tuningOptions": {"threadCount": 8}}}'
-oc -n openshift-ingress get deploy router-default -o yaml | grep " ROUTER_THREADS" -A 1
-
 echo "[INFO] run ingress-perf with thread=8, replica=4"
-export CONFIG=../../../standard-4replicas.yml
+export CONFIG=../../../standard-4replicas-8threads.yml
 run_ingress_perf
 
 echo "[INFO] run ingress-perf with thread=8, replica=3"
-export CONFIG=../../../standard-3replicas.yml
+export CONFIG=../../../standard-3replicas-8threads.yml
 run_ingress_perf
 
 echo "[INFO] run ingress-perf with thread=8, replica=2"
