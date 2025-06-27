9f4b95cd5b3e019c4591e3fbbc6482589c8fb122
Qiujie Li <81630527+qiliRedHat@users.noreply.github.com>
2025-06-27 20:26:07 +0800

    Ocp 40557 fix (#820)

--- perfscale_regression_ci/scripts/scalability/replace_loadedworker_nodes_env.sh
+++ perfscale_regression_ci/scripts/scalability/replace_loadedworker_nodes_env.sh
@@ -1,8 +1,5 @@
-source ../custom_workload_env.sh
-
-export WORKLOAD="cluster-density"
-export INDEXING=true
-export METADATA_COLLECTION=true
-export METRICS_PROFILE="metrics-profiles/metrics-aggregated.yaml"
-export COMPARISON_CONFIG="clusterVersion.json nodeMasters-max.json nodeAggWorkers.json"
-export GEN_CSV=true
+export WORKLOAD=cluster-density-v2
+export ITERATIONS=270
+export GC="false"
+export PPROF="false"
+export CHURN="false"
--- perfscale_regression_ci/utils/run_workload.sh
+++ perfscale_regression_ci/utils/run_workload.sh
@@ -2,12 +2,16 @@
 
 set -ex
 
+ES_USERNAME=${ES_USERNAME}
+ES_PASSWORD=${ES_PASSWORD}
+export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
+
 setup(){
     rm -rf e2e-benchmarking
     # Clone the e2e repo
     REPO_URL="https://github.com/cloud-bulldozer/e2e-benchmarking";
     LATEST_TAG=$(curl -s "https://api.github.com/repos/cloud-bulldozer/e2e-benchmarking/releases/latest" | jq -r '.tag_name');
-    E2E_VERSION="v2.2.5"
+    E2E_VERSION="v2.4.0"
     TAG_OPTION="--branch $(if [ "$E2E_VERSION" == "default" ]; then echo "$LATEST_TAG"; else echo "$E2E_VERSION"; fi)";
     git clone $REPO_URL $TAG_OPTION --depth 1
 }
@@ -20,9 +24,9 @@ run_workload(){
     if [[ ! -d e2e-benchmarking/workloads/kube-burner ]]; then
         setup
     fi
-    cd e2e-benchmarking/workloads/kube-burner
-    ./run.sh |& tee "kube-burner-$(date +%Y%m%d%H%M%S).out"
-    cd ../../.. #prepare for cleanup
+    pushd e2e-benchmarking/workloads/kube-burner
+    ./run.sh 2>&1 | tee "kube-burner-$(date +%Y%m%d%H%M%S).out"
+    popd
     cleanup
 }
 
@@ -31,13 +35,22 @@ run_ingress_perf(){
         setup
     fi
     pushd e2e-benchmarking/workloads/ingress-perf
-    export ES_USERNAME=${ES_USERNAME}
-    export ES_PASSWORD=${ES_PASSWORD}
-    export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
     export ES_INDEX="ingress-performance"
     export CONFIG=${CONFIG:-"config/standard.yml"}
     echo "[INFO] Will run ingress-perf with config $CONFIG"
-    ./run.sh |& tee "/tmp/ingress-perf-$(date +%Y%m%d%H%M%S).out"
+    ./run.sh 2>&1 | tee "/tmp/ingress-perf-$(date +%Y%m%d%H%M%S).out"
+    popd
+}
+
+run_kube-burner-ocp-wrapper(){
+    if [[ ! -d e2e-benchmarking/workloads/kube-burner-ocp-wrapper ]]; then
+        setup
+    fi
+    pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
+    export GC=${GC:-"false"}
+    export PPROF=${PPROF:-"false"}
+    export CHURN=${CHURN:-"false"}
+    ./run.sh 2>&1 | tee "/tmp/kube-burner-ocp-wrapper-$(date +%Y%m%d%H%M%S).out"
     popd
 }
 
