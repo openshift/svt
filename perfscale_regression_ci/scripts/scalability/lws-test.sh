80b55d322ec92efe849aad4434b1b89d75f9b339
Qiujie Li <81630527+qiliRedHat@users.noreply.github.com>
2025-10-14 21:58:55 +0800

    lws-scale-fix (#834)

--- perfscale_regression_ci/scripts/scalability/lws-test.sh
+++ perfscale_regression_ci/scripts/scalability/lws-test.sh
@@ -23,7 +23,7 @@ END
 while getopts ":n:r:s:idh" opt; do
     case ${opt} in
     n)
-        NAMESPACE=${OPTARG}
+        LWSTEST_NAMESPACE=${OPTARG}
         ;;
     r)
         REPLICAS=${OPTARG}
@@ -52,12 +52,12 @@ if [[ "$1" = "" ]];then
 fi
 
 function wait_for_running_pod {
-  echo "Waiting for all pods in namespace '$NAMESPACE' to be Running"
+  echo "Waiting for all pods in namespace '$LWSTEST_NAMESPACE' to be Running"
   RETRY=0
   max_retry=600
   START_TIME=$(date +%s)
   while true; do
-    NOT_READY=$(oc get pods -n "$NAMESPACE" --no-headers | \
+    NOT_READY=$(oc get pods -n "$LWSTEST_NAMESPACE" --no-headers | \
       awk '{print $3}' | grep -v 'Running' | wc -l)
     if [ "$NOT_READY" -eq 0 ]; then
       break
@@ -70,28 +70,29 @@ function wait_for_running_pod {
     fi
   done
   if [ "$RETRY" -gt $max_retry ]; then
-      echo "Timed out waiting for all pods in namespace '$NAMESPACE' to be Running."
+      echo "Timed out waiting for all pods in namespace '$LWSTEST_NAMESPACE' to be Running."
   else
-      echo "All pods in namespace '$NAMESPACE' are Running"
+      echo "All pods in namespace '$LWSTEST_NAMESPACE' are Running"
   fi
 
   END_TIME=$(date +%s)
   ELAPSED=$((END_TIME - START_TIME))
 
-  TOTAL_RUNNING_PODS=$(oc get pods -n "$NAMESPACE" --no-headers | grep Running | wc -l)
+  TOTAL_RUNNING_PODS=$(oc get pods -n "$LWSTEST_NAMESPACE" --no-headers | grep Running | wc -l)
 
   if [ $TOTAL_RUNNING_PODS -eq $(($REPLICAS * $SIZE)) ]; then
       echo "[PASS]" "Expected number of pods $TOTAL_RUNNING_PODS are running. Replicas: $REPLICAS, Size: $SIZE. Total pods running time: $ELAPSED seconds."
   else
       echo "[FAIL]" "Found $TOTAL_RUNNING_PODS, but expected $((REPLICAS * SIZE)) total running pods"
+      exit 1
   fi
 
   echo "START_TIME: $START_TIME, END_TIME: $END_TIME"
   echo "Statefulsets:"
-  oc get statefulsets -n "$NAMESPACE"
+  oc get statefulsets -n "$LWSTEST_NAMESPACE"
 }
 
-NAMESPACE=${NAMESPACE:-"lws-test"}
+LWSTEST_NAMESPACE=${LWSTEST_NAMESPACE:-"lws-test"}
 REPLICAS=${REPLICAS:-2}
 SIZE=${SIZE:-3}
 WORKLOAD=${WORKLOAD:-custom}
@@ -99,17 +100,17 @@ IMAGE=${IMAGE:-quay.io/cloud-bulldozer/nginx:latest}
 
 echo "=====================" 
 echo "Replicas: $REPLICAS, Size: $SIZE, Image: $IMAGE"
-echo "Running in project $NAMESPACE"
+echo "Running in project $LWSTEST_NAMESPACE"
 echo "====================="
-oc delete project "$NAMESPACE" || true
-oc new-project "$NAMESPACE" > /dev/null 2>&1
+oc delete project "$LWSTEST_NAMESPACE" || true
+oc new-project "$LWSTEST_NAMESPACE" > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-    echo "Failed to create project $NAMESPACE!"
+    echo "Failed to create project $LWSTEST_NAMESPACE!"
     exit 1
 fi
 
 if [ $WORKLOAD = "custom" ]; then
-oc apply -f -<<EOF
+  oc apply -f - <<EOF
 apiVersion: leaderworkerset.x-k8s.io/v1
 kind: LeaderWorkerSet
 metadata:
@@ -135,7 +136,7 @@ EOF
 fi
 
 if [ $WORKLOAD = "llamacpp" ]; then
-oc apply -n $NAMESPACE -f -<<EOF
+  oc apply -f - <<EOF
 apiVersion: leaderworkerset.x-k8s.io/v1
 kind: LeaderWorkerSet
 metadata:
@@ -191,6 +192,6 @@ if [[ $DELETE ]]; then
     echo "Sleep 5 minutes before deleting a pod"
     sleep 300
     echo "Start to delete a pod"
-    oc get po -n lws-test --no-headers | awk '{print $1}' | head -n 1 | xargs oc -n "$NAMESPACE" delete pod
+    oc get po -n lws-test --no-headers | awk '{print $1}' | head -n 1 | xargs oc -n "$LWSTEST_NAMESPACE" delete pod
     wait_for_running_pod
 fi
